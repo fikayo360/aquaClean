caa8bf1ca2d4e9350c5053e171a04bc1f92c4937
fikayo <fikayoadele@gmail.com>
2023-12-15 16:24:01 +0100

    main

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,8 +1,9 @@
 const Contact = () => {
     return (
-        <div id="contact" className="w-screen h-[100vh] md:h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center absolute overflow-hidden " style={{backgroundImage:'url(./contact.jpg)'}}>
+        <div id="contact" className="w-screen h-[100vh] md:h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center  overflow-hidden " style={{backgroundImage:'url(./contact.jpg)'}}>
 
-            <div  className="flex w-[90%] h-[70%] md:w-[70%] md:h-[55%] lg:h-[70%] pt-5 md:pt-3 flex-col items-center justify-center border-1 rounded-3xl bg-white relative overflow-scroll">
+            <div  className="flex w-[90%] h-[70%] md:w-[70%] md:h-[55%] lg:h-[70%] pt-3 pb-[4rem] md:pt-3 flex-col items-center justify-center border-1  bg-white rounded-3xl 
+            relative overflow-scroll">
                 <img className="absolute w-[30px] h-[30px] top-3 left-3" src="./drops.png"/>
                 <img className="absolute w-[30px] h-[30px] bottom-3 right-3" src="./drops.png"/>
                 <h1 className="text-[1.2rem] font-bold mb-1 font-pop">contact us </h1>
