5fac5565b585d36f8981016e975185787d86fdc5
fikayo <fikayoadele@gmail.com>
2023-12-15 09:31:44 +0100

    main

--- src/hero.jsx
+++ src/hero.jsx
@@ -27,8 +27,8 @@ const Hero = () => {
                 <div className="w-[80%] h-[70%] md:h-[50%] lg:w-[40%] lg:h-[60%] flex flex-col items-center justify-center p-5 border-1 rounded-3xl bg-white pt-5">
                     <h1 ref={ref} className={`text-3xl lg:text-5xl font-bold text-black uppercase mb-2 md:mb-6 lg:mb-8 ${inView?'animate__shakeX':''}`}>Aqua-Clean</h1>
                     <h1 className="text-[1rem] lg:text-2xl font-bold text-black mb-2 md:mb-6 lg:mb-5">{`we offer - ${text}`}</h1>
-                    <h1 className="text-[16px]  md:text-[1rem] lg:text-2xl text-black text-center font-bold mb-3 lg:mb-5">We provide top-notch janitorial services that will make your space shine</h1>
-                    <p className="text-[17px] md:text-[1rem] lg:text-[20px] text-black md:font-bold text-center leading-[18px] md:leading-8 lg:leading-8">
+                    <h1 className="text-[15px]  md:text-[1rem] lg:text-2xl text-black text-center font-bold mb-3 lg:mb-5">We provide top-notch janitorial services that will make your space shine</h1>
+                    <p className="text-[15px] md:text-[1rem] lg:text-[20px] text-black md:font-bold text-center leading-[18px] md:leading-8 lg:leading-8">
                     In today's dynamic business world, first impressions matter.
                      A clean and well-maintained workspace not only reflects positively on your company's image but also contributes to a healthier, more productive environment for your employees. 
                      At Aqua-Clean, we understand the profound impact of a sparkling clean space on your business success. 
