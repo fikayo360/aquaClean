a0fba2ae5f65c68ee1ae456fbc2e8a885b4c3c79
fikayo <fikayoadele@gmail.com>
2023-12-15 16:01:53 +0100

    main

--- src/team.jsx
+++ src/team.jsx
@@ -4,7 +4,7 @@ const Team = () => {
           
             <div className=" w-[80%] h-[68%] md:h-[54%] lg:w-[50%] lg:h-[45%] flex flex-col p-10 border-1 rounded-3xl bg-white ">
                 <h1 className="text-[13px] text-center md:text-[1.3rem] text-black font-bold mb-3 font-pop">Our Team: The Backbone of Cleanliness </h1>
-                <p className="text-[13px] text-black leading-[20px] md:leading-[3.2rem] lg:leading-[3.5rem] font-mont">
+                <p className="text-[13px] text-black leading-[20px] text-center md:text-start md:leading-[3.2rem] lg:leading-[3.5rem] font-mont">
                 At Aqua-Clean, we are proud to have a dedicated and passionate team of 
                 janitorial professionals who are the backbone of our success. 
                 Each member of our team brings a unique set of skills and experience to the table, united by a common goal: to provide our clients with the highest quality cleaning services possible.
