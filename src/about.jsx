1817093927370256727657eed04d28333048bdfe
fikayo <fikayoadele@gmail.com>
2023-12-15 09:38:17 +0100

    main

--- src/about.jsx
+++ src/about.jsx
@@ -5,8 +5,8 @@ const About = () => {
 
             <div className="w-[100%] lg:w-[50%] flex flex-col p-5 justify-center">
                 <div className="mb-[3rem] ">
-                    <h1 className="text-2xl text-black font-bold mb-3 text-center lg:text-left font-pop">About us</h1>
-                    <p className="text-1xl text-center lg:text-left font-mont">
+                    <h1 className="text-[1.3rem] text-black font-bold mb-3 text-center lg:text-left font-pop">About us</h1>
+                    <p className="text-[15px] text-center lg:text-left font-mont">
                     Aqua clean is a company that offers janitorial services in the kingdom of Eswatini.We are based in Mbabane.
                     Our unwavering commitment to quality and customer satisfaction has earned us a reputation as a trusted partner for businesses across diverse industries. 
                     We believe in building strong relationships with our clients, understanding their specific needs, and exceeding their expectations with every clean. 
@@ -19,21 +19,21 @@ const About = () => {
                 </div>
 
                 <div className="mb-[3rem]">
-                    <h1 className="text-2xl text-black font-bold mb-3 text-center lg:text-left font-pop"> Our mission </h1>
-                    <p className="text-1xl text-center lg:text-left font-mont"> To help Our customers have productive, safer and healthier environment through a robust range of cleaning services industry leading standards and sustainable 
+                    <h1 className="text-[1.3rem] text-black font-bold mb-3 text-center lg:text-left font-pop"> Our mission </h1>
+                    <p className="text-[15px] text-center lg:text-left font-mont"> To help Our customers have productive, safer and healthier environment through a robust range of cleaning services industry leading standards and sustainable 
                         business practices. 
                     </p>
                 </div>
 
                 <div className="mb-[3rem]">
-                    <h1 className="text-2xl text-black font-bold mb-3 text-center lg:text-left font-pop">Why choose us</h1>
-                    <p className="text-1xl text-center lg:text-left font-mont">We are professional and reliable, customer satisfaction is our priority.
+                    <h1 className="text-[1.3rem] text-black font-bold mb-3 text-center lg:text-left font-pop">Why choose us</h1>
+                    <p className="text-[15px] text-center lg:text-left font-mont">We are professional and reliable, customer satisfaction is our priority.
                     </p>
                 </div>
 
                 
                 <div className="">
-                    <h1 className="text-2xl text-black font-bold mb-3 text-center lg:text-left font-pop">Our Values</h1>
+                    <h1 className="text-[1.3rem] text-black font-bold mb-3 text-center lg:text-left font-pop">Our Values</h1>
                     <ul className="flex flex-col w-[100%] gap-2">
                         <li className="text-1xl font-mont "><span className="font-bold text-[18px] mr-2 text-center font-pop">Quality:</span>
                         We are committed to delivering exceptional cleaning services that exceed expectations.</li>
