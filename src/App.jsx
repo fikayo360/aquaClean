db5623a1be67bc6bcc38d3fb8d291c6cc2f6c1dd
fikayo <fikayoadele@gmail.com>
2023-12-15 01:54:32 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Hero from "./hero"
 import About from "./about"
 import OurServices from "./ourservices"
 import Team from "./team"
-import Testimonials from "./testimonials"
 import Contact from "./contact"
 import Footer from "./footer"
 import 'animate.css';
