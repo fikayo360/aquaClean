300b8b2824c111ff7df38327396d76f7c97ea9ec
fikayo <fikayoadele@gmail.com>
2023-12-20 14:30:28 +0100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,9 @@ import { useState,useEffect } from "react"
 
 function App() {
   
-  const [dimensions, setDimensions] = useState({
-    width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-    height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-  });
-
-  useEffect(() => {
-    const updateDimensions = () => {
-      const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-      const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-      setDimensions({ width: w, height: h });
-
-      // Set styles directly on html and body elements
-      document.documentElement.style.width = `${w}px`;
-      document.documentElement.style.height = `${h}px`;
-      document.body.style.width = `${w}px`;
-      document.body.style.height = `${h}px`;
-    };
-
-    // Set dimensions when the component mounts
-    updateDimensions();
-
-    // Update dimensions when the window is resized
-    window.addEventListener('resize', updateDimensions);
-
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', updateDimensions);
-    };
-  }, []); // Empty dependency array to run the effect only once on mount
 
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden min-h-[700px]">
      <Hero />
      <About />
      <OurServices />
