d4e56dad6ec132f8d5e894e5a37f1efc3eaaa388
fikayo <fikayoadele@gmail.com>
2024-01-03 23:16:19 +0100

    final

--- src/hero.jsx
+++ src/hero.jsx
@@ -24,11 +24,11 @@ const Hero = () => {
             {navActive?<NavMobile navActive={toggleMobileNav} />:null}
             <div className="w-[100%] h-[90%] flex flex-col justify-center lg:items-start items-center lg:pl-10"> 
 
-                <div className="w-[88%] h-[70%] md:h-[58%] lg:w-[44%] lg:h-[60%] flex flex-col items-center justify-center p-5 border-1 rounded-3xl bg-[#190E4F] pt-5">
-                    <h1 ref={ref} className={`text-3xl lg:text-3xl font-bold text-[#ecf2f9] uppercase mb-4 md:mb-6 lg:mb-6 ${inView?'animate__animated animate__rubberBand':''}`}>Aqua-Clean</h1>
+                <div className="w-[88%] h-[77%] md:h-[58%] lg:w-[44%] lg:h-[60%] flex flex-col items-center justify-center p-5 border-1 rounded-3xl bg-[#190E4F] pt-5">
+                    <h1 ref={ref} className={`text-2xl lg:text-3xl font-bold text-[#ecf2f9] uppercase mb-4 md:mb-6 lg:mb-6 ${inView?'animate__animated animate__rubberBand':''}`}>Aqua-Clean</h1>
                     <h1 className="text-[15px] md:text-[1.5rem] lg:text-[1.3rem] text-[#ecf2f9] mb-4 md:mb-6 lg:mb-3">{`we offer - ${text}`}</h1>
                     <h1 className="text-[13px]  md:text-[1rem] lg:text-[1.2rem] text-[#ecf2f9] text-center mb-2 lg:mb-5">We provide top-notch janitorial services that will make your space shine</h1>
-                    <p className="text-[13px] md:text-[1rem] lg:text-[1rem] text-[#ecf2f9] text-center leading-[2rem] md:leading-[3rem] lg:leading-7 xl:leading-8 2xl:leading-9">
+                    <p className="text-[11px] md:text-[1rem] lg:text-[1rem] text-[#ecf2f9] text-center sm:leading-2 leading-[2rem] md:leading-[3rem] lg:leading-7 xl:leading-8 2xl:leading-9">
                     In today's dynamic business world, first impressions matter.
                      A clean and well-maintained workspace not only reflects positively on your company's image but also contributes to a healthier, more productive environment for your employees. 
                      At Aqua-Clean, we understand the profound impact of a sparkling clean space on your business success. 
--- src/team.jsx
+++ src/team.jsx
@@ -8,8 +8,8 @@ const Team = () => {
         style={{backgroundImage:'url(./team.jpg)'}} >
           
             <div className=" w-[80%] h-[68%] md:h-[54%] lg:w-[50%] lg:h-[55%] xl:h-[52%] 2xl:h-[50%] flex flex-col justify-center items-center p-10 border-1 border-slate-500 rounded-3xl shadow-md bg-white ">
-                <h1 className="text-[13px] text-center md:text-[1.3rem] text-black font-bold mb-3 font-pop">Our Team: The Backbone of Cleanliness </h1>
-                <p className="text-[11px] text-black sm:leading-[35px] leading-[26px] text-center md:leading-[45px] lg:leading-[2.5rem] font-mont">
+                <h1 className="text-[12px] text-center md:text-[1.3rem] text-black font-bold mb-2 font-pop">Our Team: The Backbone of Cleanliness </h1>
+                <p className="text-[10px] text-black sm:leading-[35px] leading-[23px] text-center md:leading-[45px] lg:leading-[2.5rem] font-mont">
                 At Aqua-Clean, we are proud to have a dedicated and passionate team of 
                 janitorial professionals who are the backbone of our success. 
                 Each member of our team brings a unique set of skills and experience to the table, united by a common goal: to provide our clients with the highest quality cleaning services possible.
